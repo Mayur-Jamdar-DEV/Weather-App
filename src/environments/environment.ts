7a31af412f4ec567e38877c534501457f6ffdd5a
mayur5152 <jamdarmayur5152@gmail.com>
2023-04-16 14:02:03 +0530

    weather app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'bd734c46f2msh88ce12febfcbdbep11a7d6jsn9c383ed4b0b7'
 };
 
 /*
